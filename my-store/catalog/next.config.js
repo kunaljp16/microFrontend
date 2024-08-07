65826d0a1415f2d030355678936dcdb5fda8737d
Rupali Pardeshi <rupalipardeshi@MacBook-Pro-de-Rupali.local>
2024-08-08 00:49:07 +0200

    added rewrites and updated basepath

--- my-store/catalog/next.config.js
+++ my-store/catalog/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: '/catalog',
 }
 
 module.exports = nextConfig
--- my-store/home/next.config.js
+++ my-store/home/next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/catalog',
+        destination: 'http://localhost:3001/catalog',
+      },
+      {
+        source: '/catalog/:path*',
+        destination: 'http://localhost:3001/catalog/:path*',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
