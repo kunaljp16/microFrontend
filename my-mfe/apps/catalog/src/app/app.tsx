4b6b321c7791103647b96493fc107bfe927268ca
kunaljp16 <kunaljp16@gmail.com>
2024-09-08 23:25:58 +0200

    created recommendation app and hosted in Catalog app

--- my-mfe/apps/catalog/src/app/app.tsx
+++ my-mfe/apps/catalog/src/app/app.tsx
@@ -1,13 +1,16 @@
-import { Header } from '@ebay/ui';
+import React from 'react';
 import { Container, Header as Text } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import ProductList from './product-list';
+
+const Recommendations = React.lazy(() => import('recommendations/Module'));
 
 export function App() {
   return (
     <Container style={{ marginTop: '5em' }}>
       <Text size="huge">Catalog App</Text>
       <ProductList />
+      <Recommendations />
     </Container>
   );
 }
---
+++ my-mfe/apps/recommendations/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import 'semantic-ui-css/semantic.min.css';
+
+export function App() {
+  return (
+    <div className="ui raised segment">
+      <h1>Recommendations</h1>
+      <p>Recommmendations goes here</p>
+    </div>
+  );
+}
+
+export default App;
